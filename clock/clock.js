1160f9c58d8d458af430cb343aaca847ba021880
ROSS1996 <41270745+ROSS1996@users.noreply.github.com>
2023-01-02 15:14:46 -0300

    Add digital clock

--- clock/clock.js
+++ clock/clock.js
@@ -15,16 +15,29 @@ function updateClock() {
     let hourDeg = (currentHrs/12) * 360
     hourHand.style.transform = `rotate(${hourDeg}deg)`;
     hourHand.style.transformOrigin = 'top right';
+
+    let digitalHrs = document.getElementById('digitalHrs')
+    digitalHrs.innerText = currentHrs >= 10 ? currentHrs : `0${currentHrs}`
+
+
     // Minutes
     let currentMin = currentTime.getMinutes()
     let minHand = document.getElementById('minHand')
     let minDeg = (currentMin/60) * 360
     minHand.style.transform = `rotate(${minDeg}deg)`;
     minHand.style.transformOrigin = 'top right';
+
+    let digitalMin = document.getElementById('digitalMin')
+    digitalMin.innerText = currentMin >= 10 ? currentMin : `0${currentMin}`
+
     // Seconds
     let currentSec = currentTime.getSeconds()
     let secHand = document.getElementById('secHand')
     let secDeg = (currentSec/60) * 360
     secHand.style.transform = `rotate(${secDeg}deg)`;
     secHand.style.transformOrigin = 'top right';
+
+    let digitalSec = document.getElementById('digitalSec')
+    digitalSec.innerText = currentSec >= 10 ? currentSec : `0${currentSec}`
+
 }
