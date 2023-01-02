ce5c2333f86d1b036faf09b4be2580eb955462d9
ROSS1996 <41270745+ROSS1996@users.noreply.github.com>
2023-01-02 13:39:34 -0300

    Release clock

---
+++ clock/clock.js
@@ -0,0 +1,30 @@
+window.addEventListener('load', function() {
+    updateClock()
+    setInterval(() => {
+        updateClock()
+    }, 1000);
+})
+
+
+function updateClock() {
+    // Date Time
+    let currentTime = new Date()
+    // Hours
+    let currentHrs = currentTime.getHours()
+    let hourHand = document.getElementById('hourHand')
+    let hourDeg = (currentHrs/12) * 360
+    hourHand.style.transform = `rotate(${hourDeg}deg)`;
+    hourHand.style.transformOrigin = 'top right';
+    // Minutes
+    let currentMin = currentTime.getMinutes()
+    let minHand = document.getElementById('minHand')
+    let minDeg = (currentMin/60) * 360
+    minHand.style.transform = `rotate(${minDeg}deg)`;
+    minHand.style.transformOrigin = 'top right';
+    // Seconds
+    let currentSec = currentTime.getSeconds()
+    let secHand = document.getElementById('secHand')
+    let secDeg = (currentSec/60) * 360
+    secHand.style.transform = `rotate(${secDeg}deg)`;
+    secHand.style.transformOrigin = 'top right';
+}
